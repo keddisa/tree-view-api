34b49fd41e3633c4b3ca03a8246526cc8acbce04
Andrew <andrew@example.com>
2019-06-05 15:29:26 -0400

    limit traffic to app -correct url

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://ak-tree-frontend.herokuapp.com/");
+  res.header("Access-Control-Allow-Origin", "https://ak-tree-frontend.herokuapp.com/*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
